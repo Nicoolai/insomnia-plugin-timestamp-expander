40bc734586104841fae38c3c85790494b79ba73c
Nicolai Søndergaard <Nicolai.Sondergaard@LEGO.com>
2022-12-07 08:34:49 +0100

    Added a check for json content type so it no longer destroys binaries.

--- main.js
+++ main.js
@@ -10,19 +10,28 @@ const timeStampregex = /((["']([a-z0-9_-]+?)["']):([0-9]{10}|[0-9]{13}))([,}\s])
 module.exports.responseHooks = [
     async ctx => {
         try {
+            const contentType = ctx.response.getHeader("content-type");
+            if (!contentType.toLowerCase().includes("application/json")) {
+                return;
+            }
             const body = bufferToString(ctx.response.getBody());
             const newBody = body.replace(timeStampregex, (match, $1, $2, $3, $4, $5) => {
-                let d;
-                if ($4.length == 10) {
-                    d = new Date($4 * 1000);
+                try {
+                    let d;
+                    if ($4.length == 10) {
+                        d = new Date($4 * 1000);
+                    }
+                    else if ($4.length == 13) {
+                        d = new Date($4);
+                    }
+                    return `${$2}:${$4}, "${$3}_as_datetime":"${d.toISOString()}"${$5}`;
                 }
-                else if ($4.length == 13) {
-                    d = new Date($4);
+                catch {
+                    // Just fail silently if a value doesn't parse.
                 }
-                return `${$2}:${$4}, "${$3}_as_datetime":"${d.toISOString()}"${$5}`;
-              });
+            });
 
-              ctx.response.setBody(newBody);
+            ctx.response.setBody(newBody);
         } catch (exception) {
             console.log(exception);
         }
